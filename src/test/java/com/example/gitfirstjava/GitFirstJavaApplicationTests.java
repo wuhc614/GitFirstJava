0b4bb5bca8d18b33b81cebf411e6e4d91f0a0712
Yang <aa130577@outlook.com>
2025-06-09 01:46:16 +0800

    Init

---
+++ src/test/java/com/example/gitfirstjava/GitFirstJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitfirstjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFirstJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
