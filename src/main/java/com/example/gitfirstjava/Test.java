1fcfcd16318c788f63cc6a0b8fa53d2c58f5b01d
Yang <aa130577@outlook.com>
2025-06-09 10:28:52 +0800

    test01

---
+++ src/main/java/com/example/gitfirstjava/Test.java
@@ -0,0 +1,5 @@
+package com.example.gitfirstjava;
+
+public class Test {
+
+}
---
+++ src/main/java/com/example/gitfirstjava/TestController.java
@@ -0,0 +1,6 @@
+package com.example.gitfirstjava;
+
+public class TestController {
+
+    public static final String WORD_TEST = "sjldd";
+}
